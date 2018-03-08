23af7183b0f7246e3e88ad0e6a21caffbcccd2d5
Israel Zablianov <123hartk123@gmail.com>
2018-03-09 00:36:16 +0200

    adding content

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -1,7 +1,24 @@
 <template>
     <div class="main">
+        <div class="control-group">
+            <div class="control-btn" @click="addBox">
+                Add Box 
+            </div>
+            <div class="control-btn" @click="clearAll">
+                Clear All
+            </div>
+        </div>
         <div>
             <img :src="vueLogo" alt="Vue Logo">
+        </div>
+
+        <div class="draggable-container" v-for="boxNumber in boxes" :key="boxNumber" v-draggable>
+            Simple Draggable
+        </div>
+
+        <div class="autor">
+            <span>Autor</span>
+            Israel Zablianov
         </div>
     </div>
 </template>
@@ -9,19 +26,75 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 const vueLogo = require("../../assets/images/logo-vue.png");
-
+import { Draggable } from "draggable-vue-directive";
 @Component({
+    directives: {
+        Draggable
+    }
 })
 export default class App extends Vue { 
     vueLogo = vueLogo;
+    boxes: number = 0;
+
+    addBox() {
+        this.boxes++;
+    }
+
+    clearAll() {
+        this.boxes = 0;
+    }
 }
 </script>
 
 <style lang="scss">
 .main {
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     height: 100%;
     width: 100%;
 }
+
+.control-group {
+    display: flex;
+    flex-direction: column;
+    margin-top: 25px;
+} 
+
+.control-btn {
+    border-radius: 3px;
+    background-color: #42b883;
+    color: #35495e;
+    cursor: pointer;
+    padding: 15px;
+    margin: 15px;
+}
+
+.draggable-container {
+    width: 150px;
+    height: 150px;
+    background: white;
+    border: 3px solid #42b883;
+    border-radius: 3px;
+    text-align: center;
+    font-size: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.autor {
+    align-self: center;
+    margin-bottom: 20px;
+    padding: 10px;
+    border-radius: 3px;
+    color: #42b883;
+    background-color: #35495e;
+    font-size: 25px;
+    font-weight: bolder;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
 </style>
